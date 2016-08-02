137ee1fa13a13d5a52cc5b2c1cc7b036b7e0a045
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-08-02 23:02:34 +0200

    Add arguments check.

--- merge_tracks.py
+++ merge_tracks.py
@@ -1,5 +1,6 @@
 import gpxpy
 import gpxpy.gpx
+import sys
 
 # Parse filename and append all the points to gpx_segment.
 def append_points(filename, gpx_segment):
@@ -12,6 +13,12 @@ def append_points(filename, gpx_segment):
             for point in segment.points:
                 gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(point.latitude, point.longitude, point.elevation, point.time))
 
+if len(sys.argv)<=4:
+    print 'Merge n gpx tracks.\n'
+    print 'Syntax:'
+    print sys.argv[0]+' first.gpx second.gpx ... result.gpx\n'
+    print 'With N args: first N-1 args are the gpx to be merged, last arg is the result.'
+    sys.exit(0)
 
 gpx = gpxpy.gpx.GPX()
 
@@ -23,10 +30,10 @@ gpx.tracks.append(gpx_track)
 gpx_segment = gpxpy.gpx.GPXTrackSegment()
 gpx_track.segments.append(gpx_segment)
 
-append_points('1.gpx',gpx_segment)
-append_points('2.gpx',gpx_segment)
+for filename in sys.argv[1:-1]:
+    append_points(filename,gpx_segment)
 
 gpx_segment.points = sorted(gpx_segment.points, key=lambda p: p.time)
 
-with open('somefile.gpx', 'w') as the_file:
+with open(sys.argv[-1], 'w') as the_file:
     the_file.write(gpx.to_xml())
