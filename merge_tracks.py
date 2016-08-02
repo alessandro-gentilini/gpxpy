6f8340cdf38cfd04111a191aae640b1206b00f46
Alessandro Gentilini <alessandro.gentilini@gmail.com>
2016-08-02 22:17:04 +0200

    This is a small working snippet.

--- merge_tracks.py
+++ merge_tracks.py
@@ -1,15 +1,30 @@
 import gpxpy
 import gpxpy.gpx
 
-# Parsing an existing file:
-# -------------------------
-
-def append_points(filename):
-    gpx_file = open('test_files/cerknicko-jezero.gpx', 'r')
+# Parse filename and append all the points to gpx_segment.
+def append_points(filename, gpx_segment):
+    gpx_file = open(filename, 'r')
 
     gpx = gpxpy.parse(gpx_file)
 
     for track in gpx.tracks:
         for segment in track.segments:
             for point in segment.points:
-                print 'Point at ({0},{1}) -> {2}'.format(point.latitude, point.longitude, point.elevation)
+                gpx_segment.points.append(gpxpy.gpx.GPXTrackPoint(point.latitude, point.longitude, point.elevation, point.time))
+
+
+gpx = gpxpy.gpx.GPX()
+
+# Create first track in our GPX:
+gpx_track = gpxpy.gpx.GPXTrack()
+gpx.tracks.append(gpx_track)
+
+# Create first segment in our GPX track:
+gpx_segment = gpxpy.gpx.GPXTrackSegment()
+gpx_track.segments.append(gpx_segment)
+
+append_points('1.gpx',gpx_segment)
+
+
+with open('somefile.gpx', 'w') as the_file:
+    the_file.write(gpx.to_xml())
